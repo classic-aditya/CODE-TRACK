01d4af40696f92d0e6aca51e696269df06e0df07
classic-aditya <adityaji1906@gmail.com>
2026-04-28 03:24:25 +0530

    added env.var

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const problemSetRoutes = require("./routes/problemset.routes")
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }))
 
